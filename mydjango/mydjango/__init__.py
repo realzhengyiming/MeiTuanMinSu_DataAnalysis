8f9f2a4b2689b3f26f3b1a556d26afc17dad69ba
zhengyiming <realzhengyiming@outlook.com>
2020-09-01 20:24:26 +0800

    django3.1 配置修改

--- mydjango/mydjango/__init__.py
+++ mydjango/mydjango/__init__.py
@@ -1,3 +1,5 @@
 import pymysql
-pymysql.install_as_MySQLdb()
+pymysql.version_info = (1, 4, 13, "final", 0)
+pymysql.install_as_MySQLdb()  # 使用pymysql代替mysqldb连接数据库
+
 # python3 以上的使用pymysql 需要这样来配置  ，还有一个2.0的坑是关于MySQL连接的
